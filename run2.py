6e922f477ed635cbc0852a47bc7a5009871ea324
caochengya <875414271@qq.com>
2020-05-27 21:36:08 +0800

    新增消息

--- run2.py
+++ run2.py
@@ -4,4 +4,5 @@ from Common.send_alarm import SendAlarm
 driver = webdriver.Firefox()
 driver.get('https://www.baidu.com/')
 SendAlarm().send_dingtalk_alarm('打开浏览器了')
+print('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
 driver.close()
