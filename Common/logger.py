ee1ef42ac30acf8636b30038a711431ced592386
caochengya <875414271@qq.com>
2020-05-28 12:46:48 +0800

    关闭日志输出到控制台

--- Common/logger.py
+++ Common/logger.py
@@ -3,7 +3,7 @@ from Common import read_path
 
 # 指定日志输出位置
 fh = logging.FileHandler(read_path.log_path, encoding='utf-8')  # 输出到文件
-sh = logging.StreamHandler()  # 输出到控制台
+# sh = logging.StreamHandler()  # 输出到控制台
 
 # 指定日志输出格式
 formatter = '%(asctime)s-%(levelname)s-%(filename)s-日志信息:%(message)s'
